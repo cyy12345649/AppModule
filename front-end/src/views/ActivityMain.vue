034a497716752dc49c3ea42fd0c855bf3d69b8a7
Ming-3364 <2379637215@qq.com>
2024-05-26 21:22:58 +0800

    1. 完成买卖对话框前端，可以作为参考

--- front-end/src/views/ActivityMain.vue
+++ front-end/src/views/ActivityMain.vue
@@ -5,83 +5,76 @@
         <!-- 你的页面内容 -->
 
         <div>
-            <van-button type="primary" @click="showBuyDialog">Buy</van-button>
-                <!-- Buy dialog -->
-                <van-dialog
-                    v-model="showBuyDialogFlag"
-                    title="Buy"
-                    show-cancel-button
-                    :beforeClose="transactionBuy"
+            <!-- <van-button type="primary" @click="showBuyDialog">Buy</van-button>
+                <van-dialog v-show="showBuyDialogFlag" title="Buy" show-cancel-button :beforeClose="beforeCloseBuyDialog">
+                    <van-field v-model="buyDialogInputValue" type="digit" label="购买数量" />
+                </van-dialog> -->
+            <van-button type="primary" @click="showBuyDialogFlag = true">Buy</van-button>
+                <van-dialog 
+                    v-model="showBuyDialogFlag" 
+                    title="Sell" 
+                    show-cancel-button 
+                    @confirm="transactionBuy"
+                    @close="buyDialogInputValue=1"
                 >
-                    <!--输入框-->
-                    <van-field v-model="buyDialogInputValue" type="digit" label="购买数量" />                    
+                    <van-field v-model="buyDialogInputValue" type="digit" label="买入数量" />
                 </van-dialog>
-            <van-button type="primary" @click="showSellDialog">Sell</van-button>
-                <!-- Sell dialog -->
-                <van-dialog
-                    v-model="showSellDialogFlag"
-                    title="Sell"
-                    show-cancel-button
-                    :beforeClose="transactionSell"
+            
+            <van-button type="primary" @click="showSellDialogFlag = true">Sell</van-button>
+                <van-dialog 
+                    v-model="showSellDialogFlag" 
+                    title="Sell" 
+                    show-cancel-button 
+                    @confirm="transactionSell"
+                    @close="sellDialogInputValue=1"
                 >
-                    <!--输入框-->
-                    <van-field v-model="sellDialogInputValue" type="digit" label="卖出数量" />                    
+                    <van-field v-model="sellDialogInputValue" type="digit" label="卖出数量" />
                 </van-dialog>
         </div>
+        {{ sellDialogInputValue }}
+
     </div>
 </template>
   
 <script>
 import { getToken } from '@/utils/shared';
-import { Dialog, Toast } from 'vant';
 
 export default {
     data() {
         return {
-            // 初始化数据
-            curCoupons,
-            curPoints,
+            // ========初始化数据=====
 
-            curCouponPrice,
+            // 用户账户数据
+            curCoupons: 0,
+            curPoints: 0,
 
-            // 界面控制
-            showBuyDialogFlag: false,
-            showSellDialogFlag: false,
+            // 市场数据
+            curCouponPrice: 0,
 
+            // 用户操作数据
             buyDialogInputValue: "1",
             sellDialogInputValue: "1",
+
+            // ========界面控制=====
+
+            // 买卖dialog
+            showBuyDialogFlag: false,
+            showSellDialogFlag: false,
+            
+
+
+            // =======
+
 
         };
     },
     methods: {
-        // 你的组件方法
-        showBuyDialog() {
-            this.showBuyDialogFlag = true;            
-        },
 
-        beforeCloseBuyDialog(action, done) {
-            if (action === 'cancel') {//取消按钮
-                done();
-            } else if (action === 'confirm') {//确定按钮
-                    //向后端传值并关闭dialog弹出框
-                    var value = buyDialogInputValue;
-                    if (value !== null && value !== '') {
-                        const number = parseInt(value, 10);
-                        if (number > 0  && Number.isInteger(number)) {
-                            // await this.sendRequestToBackend(number);
-                            this.transactionBuy(number);
-                        } else {
-                            Toast.fail('输入无效，请输入xxx！');
-                        }
-                    }
-                    // this.show = false;
-                };
-                this.buyDialogInputValue="1";
-                done();
-            }
+
 
         async transactionBuy(action, done) {
             
+            var number = buyDialogInputValue;
 
             const token = getToken();
             try {
@@ -94,35 +87,8 @@ export default {
             }
         },
 
-        showSellDialog() {
-            var inputValue = ''; // 重置输入值
-            Dialog.confirm({
-                title: '输入正整数',
-                message: `请输入一个小于 ${this.curCoupons} 的正整数：`,
-                showInput: true, // 显示输入框
-                inputPlaceholder: '输入数字',
-                showCancelButton: true,
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-            })
-                .then(() => {
-                    // on confirm
-                    if (value !== null && value !== '') {
-                        const number = parseInt(value, 10);
-                        if (number > 0 && number <= this.curCoupons && Number.isInteger(number)) {
-                            // await this.sendRequestToBackend(number);
-                            this.transactionSell(number);
-                        } else {
-                            Toast.fail('输入无效，请输入一个小于 ' + this.x + ' 的正整数！');
-                        }
-                    }
-                })
-                .catch(() => {
-                    // on cancel
-                });
-        },
-
-        async transactionSell(number) {
+        async transactionSell() {
+            var number = sellDialogInputValue;
             const token = getToken();
             try {
                 const response = await this.$http.post('/api/transactionSell', { token: token, number: number });
@@ -169,3 +135,12 @@ export default {
     }
 };
 </script>
+
+<style>
+.full-screen {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+</style>
